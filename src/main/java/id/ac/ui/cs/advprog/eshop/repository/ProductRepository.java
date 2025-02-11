dce33e41f60407f36b5d10b2346ee04be2563d08
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2025-02-12 03:29:01 +0700

    changing dummy id with UUID

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -10,15 +12,8 @@ import java.util.List;
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
-    private long dummyId = 1L;
 
     public Product create(Product product) {
-
-        // if product id is still not exist
-        if (product.getProductID() == null || product.getProductID().isEmpty()) {
-            product.setProductID(String.valueOf(dummyId++));
-        }
-
         productData.add(product);
         return product;
     }
