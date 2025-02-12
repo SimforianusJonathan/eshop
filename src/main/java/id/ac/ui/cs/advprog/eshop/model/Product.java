d17659fb9107c31361fbc57baad275b31af03cbd
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2025-02-13 00:08:12 +0700

    adding validation ffor product name and product quantity input

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,13 +3,21 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
 @Setter
 public class Product {
     private String productID;
+
+    @NotBlank(message = "Product name cannot be empty or whitespace")
     private String productName;
+
+    @NotNull(message = "Product quantity cannot be null")
+    @Positive(message = "Product quantity must be a positive integer")
     private int productQuantity;
 
     public Product() {
