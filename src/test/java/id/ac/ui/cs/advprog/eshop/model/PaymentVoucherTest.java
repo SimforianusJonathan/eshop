4876d9c1d0f9362b5dfc0944ff25125ad7c30514
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2025-03-07 20:05:08 +0700

    fix : changing class name for PaymentVoucherTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherTest.java
@@ -25,42 +25,42 @@ class PaymentVoucherTest {
     @Test
     void testInvalidVoucherShorter16Length(){
         this.paymentData.put("voucherCode", "voucher10d0n15");
-        PaymentByVoucher payment = new PaymentByVoucher("13652556-012a-4c07-b546-54eb1396d79c", "VOUCHER_PAYMENT", this.paymentData);
+        PaymentVoucher payment = new PaymentVoucher("13652556-012a-4c07-b546-54eb1396d79c", "VOUCHER_PAYMENT", this.paymentData);
         assertEquals("REJECTED", payment.getStatus());
     }
 
     @Test
     void testInvalidVoucherLonger16Length(){
         this.paymentData.put("voucherCode", "voucher10d0n15451");
-        PaymentByVoucher payment = new PaymentByVoucher("13652556-012a-4c07-b546-54eb1396d79d", "VOUCHER_PAYMENT", this.paymentData);
+        PaymentVoucher payment = new PaymentVoucher("13652556-012a-4c07-b546-54eb1396d79d", "VOUCHER_PAYMENT", this.paymentData);
         assertEquals("REJECTED", payment.getStatus());
     }
 
     @Test
     void testInvalidVoucherCodePrefixEshop(){
         this.paymentData.put("voucherCode", "voucher10d0n1515");
-        PaymentByVoucher payment = new PaymentByVoucher("13652556-012a-4c07-b546-54eb1396d79e", "VOUCHER_PAYMENT", this.paymentData);
+        PaymentVoucher payment = new PaymentVoucher("13652556-012a-4c07-b546-54eb1396d79e", "VOUCHER_PAYMENT", this.paymentData);
         assertEquals("REJECTED", payment.getStatus());
     }
 
     @Test
     void testInvalidVoucherCodeLess8NumericalCharacther(){
         this.paymentData.put("voucherCode", "ESHOP1234567abcd");
-        PaymentByVoucher payment = new PaymentByVoucher("13652556-012a-4c07-b546-54eb1396d79f", "VOUCHER_PAYMENT", this.paymentData);
+        PaymentVoucher payment = new PaymentVoucher("13652556-012a-4c07-b546-54eb1396d79f", "VOUCHER_PAYMENT", this.paymentData);
         assertEquals("REJECTED", payment.getStatus());
     }
 
     @Test
     void testInvalidVoucherCodeMore8NumericalCharacther(){
         this.paymentData.put("voucherCode", "ESHOP123456789ab");
-        PaymentByVoucher payment = new PaymentByVoucher("13652556-012a-4c07-b546-54eb1396d79a", "VOUCHER_PAYMENT", this.paymentData);
+        PaymentVoucher payment = new PaymentVoucher("13652556-012a-4c07-b546-54eb1396d79a", "VOUCHER_PAYMENT", this.paymentData);
         assertEquals("REJECTED", payment.getStatus());
     }
 
     @Test
     void testVoucherCodeValid(){
         this.paymentData.put("voucherCode", "ESHOP12345678abcd");
-        PaymentByVoucher payment = new PaymentByVoucher("13652556-012a-4c07-b546-54eb1396d78b", "VOUCHER_PAYMENT", this.paymentData);
+        PaymentVoucher payment = new PaymentVoucher("13652556-012a-4c07-b546-54eb1396d78b", "VOUCHER_PAYMENT", this.paymentData);
         assertEquals("SUCCESS", payment.getStatus());
     }
 
