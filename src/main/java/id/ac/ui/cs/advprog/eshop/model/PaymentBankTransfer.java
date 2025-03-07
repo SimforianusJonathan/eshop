780321029cdaf17ef3c3e7c0737125e6bcae8e8c
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2025-03-07 12:04:12 +0700

    [REFACTOR] Apply PaymentStatus and PaymentMethod enum to PaymentBankTransfer and PaymentBankTransferTest Class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentBankTransfer.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentBankTransfer.java
@@ -1,19 +1,21 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+
 import java.util.Map;
 
 public class PaymentBankTransfer extends Payment {
     public PaymentBankTransfer(String id, String method, Map<String, String> paymentData) {
-        super(id, method, "REJECTED", paymentData); // default status before validating payment data
+        super(id, method, PaymentStatus.REJECTED.getValue(), paymentData); // default status before validating payment data
         setStatus(setValidateStatus(paymentData));
     }
 
     public String setValidateStatus(Map<String, String> paymentData) {
         if (paymentData == null || paymentData.get("bankName") == null || paymentData.get("bankName").isEmpty() ||
                 paymentData.get("referenceCode") == null || paymentData.get("referenceCode").isEmpty()) {
-            return "REJECTED";
+            return PaymentStatus.REJECTED.getValue();
         }
-        return "SUCCESS";
+        return PaymentStatus.SUCCESS.getValue();
     }
 }
 
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentBankTransferTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentBankTransferTest.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,54 +20,54 @@ class PaymentBankTransferTest {
 
     @Test
     void testInvalidEmptyPaymentData() {
-        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", this.paymentData);
+        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79b", PaymentMethod.BANK_TRANSFER.getValue(), this.paymentData);
         payment.setValidateStatus(this.paymentData);
-        assertEquals("REJECTED", payment.getStatus());
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testInvalidDataEmptyBankName() {
         this.paymentData.put("bankName", "");
         paymentData.put("referenceCode","42021269");
-        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79c", "BANK_TRANSFER", this.paymentData);
+        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79c", PaymentMethod.BANK_TRANSFER.getValue(), this.paymentData);
         payment.setValidateStatus(this.paymentData);
-        assertEquals("REJECTED", payment.getStatus());
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testInvalidDataNullBankName() {
         this.paymentData.put("bankName", null);
         paymentData.put("referenceCode","42021269");
-        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79d", "BANK_TRANSFER", this.paymentData);
+        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79d", PaymentMethod.BANK_TRANSFER.getValue(), this.paymentData);
         payment.setValidateStatus(this.paymentData);
-        assertEquals("REJECTED", payment.getStatus());
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testInvalidDataEmptyReferenceCode() {
         this.paymentData.put("bankName", "BCA");
         paymentData.put("referenceCode","");
-        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", this.paymentData);
+        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79b", PaymentMethod.BANK_TRANSFER.getValue(), this.paymentData);
         payment.setValidateStatus(this.paymentData);
-        assertEquals("REJECTED", payment.getStatus());
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testInvalidNullDeliveryFee() {
         this.paymentData.put("bankName", "BCA");
         paymentData.put("referenceCode",null);
-        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", this.paymentData);
+        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79b", PaymentMethod.BANK_TRANSFER.getValue(), this.paymentData);
         payment.setValidateStatus(this.paymentData);
-        assertEquals("REJECTED", payment.getStatus());
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testValidPaymentData() {
         this.paymentData.put("bankName", "BCA");
         paymentData.put("referenceCode","42021269");
-        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", this.paymentData);
+        PaymentBankTransfer payment = new PaymentBankTransfer("13652556-012a-4c07-b546-54eb1396d79b", PaymentMethod.BANK_TRANSFER.getValue(), this.paymentData);
         payment.setValidateStatus(this.paymentData);
-        assertEquals("SUCCESS", payment.getStatus());
+        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
     }
     
 }
